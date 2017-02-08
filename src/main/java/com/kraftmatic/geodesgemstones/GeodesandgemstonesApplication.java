d50d8a4988b0a967b88413f31d1a5e1c12628009
Nick Kraftor <kraftmatic@gmail.com>
2017-02-07 21:11:23 -0700

    first commit

---
+++ src/main/java/com/kraftmatic/geodesgemstones/GeodesandgemstonesApplication.java
@@ -0,0 +1,12 @@
+package com.kraftmatic.geodesgemstones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeodesandgemstonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeodesandgemstonesApplication.class, args);
+	}
+}
