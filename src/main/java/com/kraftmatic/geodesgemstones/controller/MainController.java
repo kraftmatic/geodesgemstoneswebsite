d96ea6044eac18438b0ecf1044ee327c7c52e663
Nicholas Kraftor <nkraftor@skytouchtechnology.com>
2017-02-19 16:32:53 -0700

    Add all database integration and shell service/repository

--- src/main/java/com/kraftmatic/geodesgemstones/controller/MainController.java
+++ src/main/java/com/kraftmatic/geodesgemstones/controller/MainController.java
@@ -1,28 +1,49 @@
 package com.kraftmatic.geodesgemstones.controller;
 
+import com.kraftmatic.geodesgemstones.database.ArticleRepository;
+import com.kraftmatic.geodesgemstones.models.Article;
+import com.kraftmatic.geodesgemstones.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
 
+	@Autowired
+    ArticleService articleService;
+
 	@RequestMapping("/")
 	public String root() {
 		return "redirect:/index";
 	}
 
-	@RequestMapping("/index")
+	@RequestMapping(path = "/index", method = RequestMethod.GET)
 	public String index(Model model) {
-		model.addAttribute("name", "Nick");
+
+		model.addAttribute("articles", articleService.retrieveLast10Articles());
 		return "index";
 	}
 
 	@RequestMapping("/user/index")
-	public String userIndex() {
+	public String userIndex(Model model) {
+		model.addAttribute("article", new Article());
 		return "user/index";
 	}
+
+
+	@RequestMapping(path = "admin/submitArticle", method = RequestMethod.POST)
+	public String articleSubmit(@ModelAttribute Article article){
+
+		articleService.saveArticle(article);
+		return "user/index";
+	}
+	/**
+	 *  Login Methods
+	 */
 
 	@RequestMapping(value = "/login")
 	public String login() {
